2fc76e8086745b7306d1433fa1a42c4582f5d25b
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-02-21 13:24:17 +0600

    added css loader for future

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
         ],
     },
     resolve: {
