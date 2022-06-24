965f3f74fb073e9f92dc6d72f1670cabd6dca69a
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-06-24 13:04:26 +0600

    update favicon

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: './src/index.html',
+            favicon: './src/assets/favicon/favicon.ico'
         }),
         new CopyPlugin({
             patterns: [
